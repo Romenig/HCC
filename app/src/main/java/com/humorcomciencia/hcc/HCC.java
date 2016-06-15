b9fb2a53b14d9407ab2b32550fde73eade4cd6e3
Romenig Ribeiro <romenig@gmail.com>
2016-06-15 16:31:06 -0300

    Subindo atualizações.

--- app/src/main/java/com/humorcomciencia/hcc/HCC.java
+++ app/src/main/java/com/humorcomciencia/hcc/HCC.java
@@ -4,23 +4,38 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class HCC extends Activity {
 
+    private boolean firstLoadAccomplished = false;
+    private ProgressBar progress;
+    private  WebView browser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hcc);
-        WebView browser = (WebView) findViewById(R.id.webView);
+        browser = (WebView) findViewById(R.id.webView);
         browser.setBackgroundColor(0x00000000);
         WebSettings webSettings = browser.getSettings();
         webSettings.setJavaScriptEnabled(true);
         browser.setWebViewClient(new MyBrowser());
+        browser.setWebChromeClient(new MyChrome());
         browser.loadUrl("http://www.humorcomciencia.com");
+        progress = (ProgressBar) findViewById(R.id.progressBar);
+        progress.setMax(100);
+    }
 
+    private class MyChrome extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            HCC.this.setValue(newProgress);
+            super.onProgressChanged(view, newProgress);
+        }
     }
 
     private class MyBrowser extends WebViewClient {
@@ -33,13 +48,25 @@ public class HCC extends Activity {
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
-            view.setVisibility(View.INVISIBLE);
+            HCC.this.progress.setProgress(0);
+            HCC.this.progress.setVisibility(View.VISIBLE);
+            if(!firstLoadAccomplished){
+                view.setVisibility(View.INVISIBLE);
+            }
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-            view.setVisibility(View.VISIBLE);
+            HCC.this.progress.setVisibility(View.INVISIBLE);
+            if(!firstLoadAccomplished){
+                view.setVisibility(View.VISIBLE);
+                firstLoadAccomplished = true;
+            }
         }
+    }
+
+    public void setValue(int progress) {
+        this.progress.setProgress(progress);
     }
 }
